89fafe1c0aa7adef90288b53eafbb140085775f7
guilhermelendines <162458067+guilhermelendines@users.noreply.github.com>
2024-03-12 11:54:22 -0300

    Add files via upload

--- script.sql
+++ script.sql
@@ -1,12 +1,30 @@
-SELECT * FROM tb_usuario;
-
-INSERT INTO tb_usuario(login, senha) VALUES 
-('admin', 'admin'),
-('joao', '123456');
-
-
-CREATE TABLE tb_usuario(
-	cod_usuario SERIAL PRIMARY KEY,
-	login VARCHAR(200) NOT NULL,
-	senha VARCHAR(200) NOT NULL	
-);
+DO $$
+DECLARE
+	n1 NUMERIC(5, 2);
+	n2 INTEGER;
+	limite_inferior INTEGER := 5;
+	limite_superior INTEGER := 17;
+BEGIN
+	-- 0 <= n <1 (intervalo real)
+	n1 := random();
+	RAISE NOTICE 'N1: %', n1;
+END;
+$$
+-- DECLARE
+-- 	codigo INTEGER :=1;
+-- 	nome_completo VARCHAR(200) := 'João Santos';
+-- 	salario NUMERIC(11,2):= 20.5;
+-- BEGIN
+-- 	RAISE NOTICE'Meu código é %, me chamo % e meu salário é %',
+-- 	codigo,nome_completo,salario;
+-- END;
+$$
+BEGIN
+-- DO
+-- $$
+--     BEGIN 
+-- 	--placeholders
+-- 	RAISE NOTICE '% + % = %', 2,2,2,+2;
+-- 	RAISE NOTICE 'Meu primeiro bloco anônimo';
+-- END;
+-- $$
